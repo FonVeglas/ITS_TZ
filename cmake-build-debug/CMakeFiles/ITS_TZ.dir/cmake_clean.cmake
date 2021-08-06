4a15975e60340580d1aba361f94c10896aecaed3
Ilya Gaponov <gaponov200@gmail.com>
2021-08-06 17:27:12 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ITS_TZ.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ITS_TZ.dir/main.cpp.obj"
+  "ITS_TZ.exe"
+  "ITS_TZ.exe.manifest"
+  "ITS_TZ.pdb"
+  "libITS_TZ.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ITS_TZ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,86 @@
+#define CURL_STATICLIB
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <curl/curl.h>
+
+
+struct Date {
+    std::string day;
+    std::string monthStr;
+    std::string monthNum;
+    std::string year;
+
+    std::string hours;
+    std::string minutes;
+    std::string seconds;
+};
+
+std::string MonthStringToNum(const Date &date) {
+    if (date.monthStr == "Jan")
+        return "01";
+    if (date.monthStr == "Feb")
+        return "02";
+    if (date.monthStr == "Mar")
+        return "03";
+    if (date.monthStr == "Apr")
+        return "04";
+    if (date.monthStr == "May")
+        return "05";
+    if (date.monthStr == "Jun")
+        return "06";
+    if (date.monthStr == "Jul")
+        return "07";
+    if (date.monthStr == "Aug")
+        return "08";
+    if (date.monthStr == "Sep")
+        return "09";
+    if (date.monthStr == "Oct")
+        return "10";
+    if (date.monthStr == "Nov")
+        return "11";
+    if (date.monthStr == "Dec")
+        return "12";
+    return "00";
+}
+
+int main(int argc, char *argv[]) {
+    CURL *curl;
+    CURLcode res;
+    FILE *file = fopen("verbose.txt", "w+");
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+        curl_easy_setopt(curl, CURLOPT_STDERR, file);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, nullptr);
+        res = curl_easy_perform(curl);
+        fclose(file);
+        curl_easy_cleanup(curl);
+    }
+    std::fstream input("verbose.txt");
+    std::stringstream dateStream;
+    std::string dateString;
+    while (std::getline(input, dateString)) {
+        if (dateString.find("Date") != -1)
+            break;
+    }
+    Date date;
+    dateString.erase(dateString.begin(), dateString.begin() + 13);
+    dateStream << dateString;
+    std::getline(dateStream, date.day, ' ');
+    std::getline(dateStream, date.monthStr, ' ');
+    date.monthNum = MonthStringToNum(date);
+    std::getline(dateStream, date.year, ' ');
+    std::getline(dateStream, date.hours, ':');
+    std::getline(dateStream, date.minutes, ':');
+    std::getline(dateStream, date.seconds, ' ');
+
+
+    //system("date " + date.day + date.monthNum + date.year); //ВОТ ОНО
+    //system(dateSys); //ВОТ ОНО
+    //system("pause");
+    return 0;
+}
